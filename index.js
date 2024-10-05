1d0e9895f18756347ed3000068f895c2d61b8139
advaohana <adva1230@gmail.com>
2024-10-06 00:06:37 +0300

    Manager Page added style and visibility to the manager page and a button that go back to home page

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
